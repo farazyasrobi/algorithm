1f6eca60a01d9f9474875b7b0d9027f45461c728
Faraz Yasrobi <farazyasrobi@gmail.com>
2016-08-21 15:32:28 -0400

    Week 1 completed

---
+++ coursera/APlusB/APlusB/APlusB.cpp
@@ -0,0 +1,17 @@
+// APlusB.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main() {
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	std::cin >> a;
+	std::cin >> b;
+	sum = a + b;
+	std::cout << sum;
+	return 0;
+}
+
